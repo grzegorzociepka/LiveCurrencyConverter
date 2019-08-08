2d424a1892b2d886da7606a5f58c6e4771b9ad2c
Grzesiek <grzegorzociepka09@gmail.com>
2019-08-08 21:54:14 +0200

    added gitignore

--- LiveCurrencyConverter/DTO/RateDTO.cs
+++ LiveCurrencyConverter/DTO/RateDTO.cs
@@ -2,6 +2,9 @@ namespace LiveCurrencyConverter.DTO
 {
     public class RateDTO
     {
-        
+        public string Currency { get; set; }
+        public string Code { get; set; }
+        public decimal Bid { get; set; }
+        public decimal Set { get; set; }
     }
 }
