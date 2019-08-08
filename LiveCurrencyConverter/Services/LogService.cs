04e04d65fdcf6c5a3f4e40d18602f73726fc25a9
Grzesiek <grzegorzociepka09@gmail.com>
2019-08-08 22:56:34 +0200

    added implementation in logService

--- LiveCurrencyConverter/Services/LogService.cs
+++ LiveCurrencyConverter/Services/LogService.cs
@@ -1,7 +1,18 @@
+using LiveCurrencyConverter.DTO;
+using LiveCurrencyConverter.Entities;
+using LiveCurrencyConverter.Repository;
+using LiveCurrencyConverter.Services.Interfaces;
+
 namespace LiveCurrencyConverter.Services
 {
-    public class LogService
+    public class LogService : ILogService
     {
-        
+        private readonly ApplicationContext _context;
+        public void AddLog(LogDTO logDto)
+        {
+            var logToInsert = new Log(logDto);
+            _context.Logs.Add(logToInsert);
+            _context.SaveChanges();
+        }
     }
 }
--- LiveCurrencyConverter/Services/NBPApiService.cs
+++ LiveCurrencyConverter/Services/NBPApiService.cs
@@ -6,7 +6,12 @@ namespace LiveCurrencyConverter.Services
 {
     public class NBPApiService : INBPApiService
     {
-        
+        private readonly ILogService _logService;
+
+        public NBPApiService(ILogService logService)
+        {
+            _logService = logService;
+        }
         public List<RateDTO> getRates()
         {
             throw new System.NotImplementedException();
