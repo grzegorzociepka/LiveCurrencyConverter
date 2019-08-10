42307262f162aaad99afd9978b26f4cb157634a8
Grzesiek <grzegorzociepka09@gmail.com>
2019-08-10 16:11:02 +0200

    added ctr in log service, logs in service methods

--- LiveCurrencyConverter/Services/LogService.cs
+++ LiveCurrencyConverter/Services/LogService.cs
@@ -8,6 +8,11 @@ namespace LiveCurrencyConverter.Services
     public class LogService : ILogService
     {
         private readonly ApplicationContext _context;
+
+        public LogService(ApplicationContext context)
+        {
+            _context = context;
+        }
         public void AddLog(LogDTO logDto)
         {
             var logToInsert = new Log(logDto);
--- LiveCurrencyConverter/Services/NBPApiService.cs
+++ LiveCurrencyConverter/Services/NBPApiService.cs
@@ -24,6 +24,7 @@ namespace LiveCurrencyConverter.Services
             var client = new HttpClient();
             client.BaseAddress = baseAddress;
             var request = await client.GetAsync(string.Format("tables/c"));
+            addLog("Get all rates request has been sent");
             var response = await request.Content.ReadAsStringAsync();
             if (request.IsSuccessStatusCode)
             {
@@ -33,9 +34,7 @@ namespace LiveCurrencyConverter.Services
             }
             else
             {
-                var log = new LogDTO();
-                log.Description = "Bad request";
-                _logService.AddLog(log);
+                addLog("External - Bad request on getRates");
                 return new List<RateDTO>();
             }
         }
@@ -44,10 +43,12 @@ namespace LiveCurrencyConverter.Services
         {
             var firstRate = await getRateToCalculate(from);
             var secondRate = await getRateToCalculate(to);
-            var sold = firstRate.Bid * amount;
-            var bought = secondRate.Set * sold;
-
-            return bought;
+            var sold = firstRate.Ask * amount;
+            var bought = sold / secondRate.Bid;
+            
+            addLog(string.Format("Internal - Conversion from {0} to {1}, amount {2}",from,to,amount));
+            
+            return Math.Round(bought, 4);
         }
 
         private async Task<RateDTO> getRateToCalculate(string from)
@@ -55,8 +56,23 @@ namespace LiveCurrencyConverter.Services
             var client = new HttpClient();
             client.BaseAddress = baseAddress;
             var request = await client.GetAsync(string.Format("rates/c/{0}", from));
+            addLog(string.Format("External - Get rate with code {0}",from));
+            
             var response = await request.Content.ReadAsStringAsync();
-            return JsonConvert.DeserializeObject<RateDTO>(response);
+            var deserializedResponse = JsonConvert.DeserializeObject<SingleRateResponseDTO>(response);
+            
+            var rateToReturn = new RateDTO();
+            rateToReturn.Bid = deserializedResponse.Rates.FirstOrDefault().Bid;
+            rateToReturn.Ask = deserializedResponse.Rates.FirstOrDefault().Ask;
+            rateToReturn.Code = deserializedResponse.Code;
+            return rateToReturn;
+        }
+
+        private void addLog(string desc)
+        {
+            var log = new LogDTO();
+            log.Description = desc;
+            _logService.AddLog(log);
         }
     }
 }
